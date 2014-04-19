68d292633f553ba9e3a9b63e8e6d6ab1b024c64d
Aleksandr Filatov <greybax@gmail.com>
2014-04-19 20:04:22 +0000

    add checkEntropy function

--- password.js
+++ password.js
@@ -12,8 +12,8 @@ var Password = (function() {
 	var _digits              = "0123456789";
 	var _alphabet            = "abcdefghijklmnopqrstuvwxyz";
 	var _alphabetCapitalized = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	var _spec                = "-_!@#$%^&*()+/{[]}|\;:'<>?";
-
+	var _spec                = "~`-_!@#$%^&*()+/{[]}|\\;:'<>?";
+	
 	var _randInt = function(min, max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	};
@@ -29,7 +29,7 @@ var Password = (function() {
 			chars += _alphabetCapitalized;
 		if (useSpec)
 			chars += _spec;
-			
+
 		return chars;
 	};
 	
@@ -53,13 +53,13 @@ var Password = (function() {
 			// generated string
 			password += c;
 		}
-		
+
 		// return the password
 		return password;
 	};
 
 	return {
-	
+
 		/***************************************
 		*    Common config settings 
 		* *************************************/
@@ -96,7 +96,52 @@ var Password = (function() {
 				);
 			}
 			
-			return JSON.stringify(passwordArray);			
+			return JSON.stringify(passwordArray);
+		},
+		
+        /***************************************
+		*    Public common method 
+		*    for calculation password entropy
+		* *************************************/
+		checkEntropy: function(password) {
+			if (password.length < 0) {
+				return 0;
+			}
+			
+			var alphabetChars = _alphabet.length;
+			var alphabetCapitalizedChars = _alphabetCapitalized.length;
+			var specChars = _spec.length;
+			var digitChars = _digits.length;
+			
+			var fAlphabet = false;
+			var fAlphabetCapitalized = false;
+			var fSpec = false;
+			var fDigit = false;
+			var charset = 0;
+
+			for (var i = 0; i < password.length; i++) {
+				var c = password.charAt(i);
+
+				if (_alphabet.indexOf(c) != -1)
+					fAlphabet = true;
+				else if (_alphabetCapitalized.indexOf(c) != -1)
+					fAlphabetCapitalized = true;
+				else if (_spec.indexOf(c) != -1)
+					fSpec = true;
+				else if (_digits.indexOf(c) != -1)
+					fDigit = true;
+			}
+		   
+			if (fAlphabet)
+				charset += alphabetChars;
+			if (fAlphabetCapitalized)
+				charset += alphabetCapitalizedChars;
+			if (fSpec)
+				charset += specChars;
+			if (fDigit)
+				charset += digitChars;
+			
+			return Math.floor(Math.log(charset) * (password.length / Math.log(2)));
 		}
 	}
 }());
